519848a2ab5137c9c7ef66df0f938dd77b82bef1
chai-wit <atpolrd@chaiwit.ac.th>
2025-09-09 17:09:43 +0700

    6000

--- script.js
+++ script.js
@@ -471,7 +471,7 @@
             event.preventDefault();
 
             // Show loading alert (modal) for saving
-            showAppAlert('กำลังบันทึกข้อมูล...', 'info', 'modal', true, 5000);
+            showAppAlert('กำลังบันทึกข้อมูล...', 'info', 'modal', true, 6000);
 
             // Get current date and time for transferDate and transferTime
             // These values are already set in the readonly inputs, but we'll re-get them to be safe
